32b67ce1b4f5558576ff9a64534ba71690372810
kundan <kundan.roy@motifworks.com>
2018-01-28 17:33:08 +0530

    design

--- modules/Admin/views/layouts/header.blade.php
+++ modules/Admin/views/layouts/header.blade.php
@@ -13,8 +13,6 @@
         <!-- BEGIN GLOBAL MANDATORY STYLES -->
         <link href="https://fonts.googleapis.com/css?family=Muli:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
         
-        <link href="{{ asset('assets/css/bootstrap.min.css')}}" rel="stylesheet">
-        <link href="{{ asset('assets/css/vendimation-style-270118.css')}}" rel="stylesheet">
         <link href="{{ URL::asset('assets/css/style.css')}}" rel="stylesheet">
     </head>
 <body>
