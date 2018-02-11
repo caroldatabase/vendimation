39619ad253a0cc93b96a404eb63b9d4e0dc78d6c
kundan <kundan.roy@motifworks.com>
2018-02-11 14:41:32 +0530

    tnc

--- modules/Admin/views/partials/my-wallet.blade.php
+++ modules/Admin/views/partials/my-wallet.blade.php
@@ -2,7 +2,7 @@
 <h4>My Wallet</h4>
     
     @foreach($card as $result)
-    <div class="personal-box work-detail product-view wallet">
+    <div class="personal-box work-detail product-view wallet" >
         <div class="row">
             <div class="col-sm-6">
                 <p class="wallet-in"><a href="#"><img src="{{ asset('assets/img/circle-right.jpg')}}"></a> <span class="nam-card">{{$result->first_name}}<span>****{{substr($result->card_number,-4)}}</span></span></p>
